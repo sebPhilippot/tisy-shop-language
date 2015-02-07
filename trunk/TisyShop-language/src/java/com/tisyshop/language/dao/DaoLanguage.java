d7ebba6a332ee484b7d602e52b27f90944f5c135
mogno.stephane@gmail.com <mogno.stephane@gmail.com@97e49a2f-4fbd-36d5-59a4-c5945b56fdb7>
2015-02-07 17:22:04 +0000

    dao language

---
+++ trunk/TisyShop-language/src/java/com/tisyshop/language/dao/DaoLanguage.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tisyshop.language.dao;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author smo    
+ */
+public interface DaoLanguage {
+    public ArrayList<String> getLanguage();
+}
